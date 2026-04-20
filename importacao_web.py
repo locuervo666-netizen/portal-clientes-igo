751af6d0b9c254b6ed092a4b85c018c146cfd759
locuervo666-netizen <locuervo666@gmail.com>
2026-04-20 14:15:24 -0300

    Update importacao_web.py

--- importacao_web.py
+++ importacao_web.py
@@ -651,8 +651,7 @@ if menu == "📊 GRID":
 
 # 🔥 ESCONDE O CABEÇALHO GLOBAL SE ESTIVER NO DASHBOARD (PARA CABER NA TV) 🔥
 if menu != "📈 Dashboard":
-    st.markdown(f"""<div class="header-container"><h2 style="margin:0; font-weight:900; font-size:24px; color:#0F172A;">Central de Controle Operacional</h2><div class='sync-status'>🟢 Online: {datetime.now(FUSO_BR).strftime('%H:%M')}</div></div>""", unsafe_allow_html=True)
-
+    
 st.markdown(f"""<div class="header-container"><h2 style="margin:0; font-weight:900; font-size:24px; color:#0F172A;">Central de Controle Operacional</h2><div class='sync-status'>🟢 Online: {datetime.now(FUSO_BR).strftime('%H:%M')}</div></div>""", unsafe_allow_html=True)
 
 # =============================================================================
